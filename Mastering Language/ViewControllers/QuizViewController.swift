5e0094ef692e17467292441447e04b9cb39856ed
vaibhavs95 <vaibhavsingh911@gmail.com>
2017-09-15 13:57:35 +0530

    initial commit

---
+++ Mastering Language/ViewControllers/QuizViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuizViewController.swift
+//  Mastering Language
+//
+//  Created by Vaibhav Singh on 15/09/17.
+//  Copyright © 2017 Moldedbits. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
