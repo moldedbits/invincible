802041805b1cac909dbe504c15ca2db1106f6008
7rcksh@gmail.com <7rcksh@gmail.com>
2017-09-15 14:06:37 +0530

    Create PassageViewController.

---
+++ Mastering Language/ViewControllers/PassageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PassageViewController.swift
+//  Mastering Language
+//
+//  Created by Koushal Sharma on 15/09/17.
+//  Copyright © 2017 Moldedbits. All rights reserved.
+//
+
+import UIKit
+
+class PassageViewController: UIViewController {
+
+    override func viewDidLoad() {
+    }
+}
